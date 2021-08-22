7049d98447ca56640c7cf756ef6ecb39a4e3e028
trigg <triggerhapp@gmail.com>
2021-08-22 20:37:24 +0100

    - Remove unnecessary log

--- client/preload-browser.js
+++ client/preload-browser.js
@@ -8,7 +8,6 @@ contextBridge.exposeInMainWorld(
             ipcRenderer.send(channel, data);
         },
         recv: (channel, func) => {
-            console.log("ON " + channel);
             ipcRenderer.on(channel, (event, ...args) => func(...args));
         }
     }
